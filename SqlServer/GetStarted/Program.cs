03c7bdabf5263dddb90191d36081f9eb261e6d0e
Igor Tkachev <igor.tkachev@gmail.com>
2013-06-07 20:08:55 -0400

    New SqlServer/GetStarted demo project.

---
+++ SqlServer/GetStarted/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Linq;
+
+namespace GetStarted
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			using (var db = new DataModel.NorthwindDB())
+			{
+				var q =
+					from c in db.Customers
+					select c;
+
+				foreach (var c in q)
+					Console.WriteLine(c.ContactName);
+			}
+		}
+	}
+}
