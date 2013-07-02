399f1a2cabfaf295a85bd3eaccdd8e4ffbc95af1
Igor Tkachev <igor.tkachev@gmail.com>
2013-07-02 16:11:20 -0400

    MySql/GetStarted project.

---
+++ MySql/GetStarted/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Linq;
+
+namespace GetStarted
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			using (var db = new DataModel.testdataDB())
+			{
+				var q =
+					from c in db.people
+					select c;
+
+				foreach (var c in q)
+					Console.WriteLine(c.PersonID);
+			}
+		}
+	}
+}
---
+++ MySql/GetStarted/README.md
@@ -0,0 +1,2 @@
+1. Install the [linq2db.MySql](https://nuget.org/packages/linq2db.MySql/) nuget.
+2. Open [LinqToDB.Templates\CopyMe.MySql.tt.txt](https://github.com/linq2db/examples/blob/master/MySql/GetStarted/LinqToDB.Templates/CopyMe.MySql.tt.txt) and follow the instructions inside the file.
