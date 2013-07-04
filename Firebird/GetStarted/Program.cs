da6884fc0979252e21e13beca26cb293702a4f97
Igor Tkachev <igor.tkachev@gmail.com>
2013-07-04 16:30:35 -0400

    Firebird/GetStarted project.

---
+++ Firebird/GetStarted/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Linq;
+
+namespace GetStarted
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			using (var db = new DataModel.TestDataDB())
+			{
+				var q =
+					from c in db.People
+					select c;
+
+				foreach (var c in q)
+					Console.WriteLine(c.PERSONID);
+			}
+		}
+	}
+}
