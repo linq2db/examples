1fed1c383189386e623585e17c8a821aad3d56ff
Igor Tkachev <igor.tkachev@gmail.com>
2015-08-16 23:15:27 -0400

    SqlServer2005 example.

---
+++ SqlServer/SqlServer2005/SqlServer2005/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Diagnostics;
+
+using DataModels;
+
+using LinqToDB;
+using LinqToDB.Data;
+
+namespace SqlServer2005
+{
+	class Program
+	{
+		static void Main()
+		{
+			DataConnection.TurnTraceSwitchOn();
+			DataConnection.WriteTraceLine = (s, s1) => Debug.WriteLine(s, s1);
+
+			using (var db = new ExampleDB())
+			{
+				db.TestTables.Insert(() => new TestTable
+				{
+					CreatedOn = DateTime.Now
+				});
+			}
+		}
+	}
+}
