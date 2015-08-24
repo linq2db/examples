e78e497f4fdfad4a7f6eea5e26893f8fa2c61499
Igor Tkachev <igor.tkachev@gmail.com>
2015-08-23 23:25:19 -0400

    DataConnectionDemo.

---
+++ DataConnection/DataConnectionDemo/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Diagnostics;
+using System.Linq;
+
+using static System.Console;
+
+using LinqToDB.Data;
+
+namespace DataConnectionDemo
+{
+	using DataModels;
+		
+	class Program
+	{
+		static void Main()
+		{
+#if DEBUG
+			DataConnection.TurnTraceSwitchOn();
+			DataConnection.WriteTraceLine = (s, s1) => Debug.WriteLine(s, s1);
+#endif
+
+			NorthwindTest();
+		}
+
+		static void NorthwindTest()
+		{
+			using (var db = new NorthwindDB())
+			{
+				var q =
+					from order in db.Orders
+					select new
+					{
+						order.Customer.CompanyName,
+						Count = order.OrderDetails.Count(),
+					};
+
+				foreach (var item in q)
+				{
+					WriteLine($"Company Name: {item.CompanyName}, Count: {item.Count}");
+				}
+			}
+		}
+	}
+}
