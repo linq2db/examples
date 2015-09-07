79a71cf44e233e01689bb55c6410227f89f4e12e
Igor Tkachev <igor.tkachev@gmail.com>
2015-09-07 17:04:32 -0400

    Managed Oracle project.

---
+++ Oracle/ManagedTest/Managed/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Diagnostics;
+
+using LinqToDB;
+using LinqToDB.Data;
+using LinqToDB.Mapping;
+
+namespace Managed
+{
+	using DataModels;
+
+	class Program
+	{
+		[Table]
+		public class TestTable
+		{
+			[PrimaryKey, Identity]           public int       ID;
+			[Column(Length = 50), NotNull]   public string    Name;
+			[Column(Length = 250), Nullable] public string    Description;
+			[Column]                         public DateTime? CreatedOn;
+		}
+
+		static void Main()
+		{
+			DataConnection.TurnTraceSwitchOn();
+			DataConnection.WriteTraceLine = (s,s1) => Debug.WriteLine(s,s1);
+
+			using (var db = new TestDataDB())
+			{
+				var table = db.CreateTable<TestTable>();
+
+				var identity = db.GetTable<TestTable>()
+					.InsertWithIdentity(() => new TestTable
+					{
+						Name = "Crazy Frog",
+						CreatedOn = Sql.CurrentTimestamp
+					});
+
+				Console.WriteLine(identity);
+
+				table.Drop();
+			}
+		}
+	}
+}
